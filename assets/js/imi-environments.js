bb07c8a47e1b309f1585f6932c2b2c2770d9cbe4
villarrealed <ed.villarreal@gmail.com>
2026-04-28 15:50:23 -0700

    feat: replace IMI widget with Webex Connect JS SDK for native in-app chat
    - Added Webex Connect JS SDK (v1.8.0) with imi-environments.js config
    - Built native chat UI inside phone shell using SDK messaging
    - SDK flow: startup → register → connect → createThread → publish/receive
    - Chat messages render as native iOS-style bubbles within the app
    - Removed old IMI widget iframe approach (chat-frame.html)

---
+++ assets/js/imi-environments.js
@@ -0,0 +1,30 @@
+var imiEnvironments = {
+    "target": "default",
+    "default": {
+        "asset": {
+            "appId": 'ED23175248',
+            "appSecret": '6LWlArv9',
+            "pathConfig": {
+                "assetPath": "/farmers-insurance-demo/assets/",
+                "root": "/farmers-insurance-demo/"
+            }
+        },
+        "imiclient": {
+            "shouldRequestNotificationPermission": false,
+            "config": {},
+            "imipush": {
+                "safariWebPushId": 'web.com.imiconnect.safari.webpush'
+            },
+            "authdomain": 'https://rtm-us.imiconnect.io/rtmsAPI',
+            "rtmsdomain": 'rtm-us.imiconnect.io',
+            "safariRegisterURL": 'https://rtm-us.imiconnect.io/apnpweb/'
+        },
+        "sw": {
+            "config": {
+                "messagingSenderId": '',
+                "serverUrl": 'https://rtm-us.imiconnect.io',
+                "appid": 'ED23175248'
+            }
+        }
+    }
+}
